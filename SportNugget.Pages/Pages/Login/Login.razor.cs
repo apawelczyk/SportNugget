5d4b0dcdea3ddccc3e45b7b8b074696e5db4d8ec
Your Name <you@example.com>
2022-04-12 16:11:19 -0500

    + Login and top menu TODO: Setup Identity Server and Auth state

---
+++ SportNugget.Pages/Pages/Login/Login.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using SportNugget.Logging.Interfaces;
+using SportNugget.Pages.Shared;
+
+namespace SportNugget.Pages.Pages.Login
+{
+    public partial class Login : PageBase
+    {
+        #region Injected Services
+        [Inject]
+        public ILogger Logger { get; set; }
+        #endregion
+
+        #region Parameters
+        #endregion
+
+        #region Local Variables
+        #endregion
+
+        #region Lifecycles
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Logger.LogInfo("Login.razor.cs OnInitializedAsync!");
+            }
+            catch (Exception e)
+            {
+                Error?.ProcessError(e);
+            }
+        }
+        #endregion
+
+        #region Tasks
+        #endregion
+    }
+}
