5c22603f53163954e23a8b01cefaeb8575b7d681
Your Name <you@example.com>
2022-04-26 14:08:11 -0500

    + Identity server migration backup

--- SportNugget.Identity/Helpers/ServiceCollectionHelper.cs
+++ SportNugget.Identity/Helpers/ServiceCollectionHelper.cs
@@ -1,16 +1,19 @@
 ﻿using SportNugget.Identity.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportNugget.Identity.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportNugget.Identity.Helpers
 {
     public static class ServiceCollectionHelper
     {
-        public static void InitializeIdentityServer(this IServiceCollection services)
+        public static void InitializeIdentityServer(this IServiceCollection services, IConfiguration configuration)
         {
             //services.AddIdentity<ApplicationUser, IdentityRole>()
             //    .AddEntityFrameworkStores<ApplicationDbContext>()
             //    .AddDefaultTokenProviders();
+
+            var migrationAssembly = typeof(WebApplication).GetType().Assembly.GetName().Name;
 
             services.AddIdentityServer(options =>
             {
@@ -31,7 +34,17 @@ namespace SportNugget.Identity.Helpers
             .AddInMemoryClients(Config.GetClients())
             .AddInMemoryApiScopes(Config.GetApiScopes())
             .AddTestUsers(Config.GetUsers().ToList())
-            .AddProfileService<ProfileService>();
+            .AddProfileService<ProfileService>()
+            .AddConfigurationStore(opt =>
+            {
+                opt.ConfigureDbContext = c => c.UseSqlServer(configuration.GetConnectionString($"SportNugget"),
+                    sql => sql.MigrationsAssembly(migrationAssembly));
+            })
+            .AddOperationalStore(opt =>
+            {
+                opt.ConfigureDbContext = o => o.UseSqlServer(configuration.GetConnectionString($"SportNugget"),
+                    sql => sql.MigrationsAssembly(migrationAssembly));
+            });
         }
     }
 }
--- SportNugget.Identity/Program.cs
+++ SportNugget.Identity/Program.cs
@@ -4,9 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.InitializeIdentityServer();
+builder.Services.InitializeIdentityServer(builder.Configuration);
 
 var app = builder.Build();
+
+//app.MigrateDatabase();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
